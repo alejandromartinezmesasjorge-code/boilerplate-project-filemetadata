7997ef8b2b6418be61316f4c02ac2311f4c8b3dc
alejandromartinezmesasjorge-code <alejandromartinezmesasjorge@gmail.com>
2026-07-09 02:57:13 +0000

    Final FCC

--- index.js
+++ index.js
@@ -7,29 +7,20 @@ const multer = require('multer');
 const app = express();
 
 app.use(cors());
-
 app.use('/public', express.static(process.cwd() + '/public'));
 
-const upload = multer({
-  dest: 'uploads/'
-});
+const upload = multer();
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
-
-  if (!req.file) {
-    return res.status(400).json({ error: 'No file uploaded' });
-  }
-
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
     size: req.file.size
   });
-
 });
 
 const listener = app.listen(process.env.PORT || 3000, function() {
